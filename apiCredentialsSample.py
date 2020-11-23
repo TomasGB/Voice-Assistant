a7379e522f0be9c38fbfe985bcf0fd661bf2c641
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2020-11-23 18:47:35 -0300

    apiCredentials sample file

---
+++ apiCredentialsSample.py
@@ -0,0 +1,18 @@
+
+#OpenWeather API
+weather_Key = 'your weather_Key'
+
+#Twitch API
+
+twitchUser_ID = 'your twitchUser_ID'
+
+twitchUser_SECRET = 'your twitchUser_SECRET'
+
+#Twitter API
+twitter_key='your twitter_key'
+
+twitter_keySecret = 'your twitter_keySecret'
+
+twitter_pin = 'your twitter_pin'
+twitter_acces_token = 'your twitter_acces_token'
+twitter_acces_tokenSecret = 'twitter_acces_tokenSecret'
